85eef0427c9353df3a3f964a7fd0aebd04cfc664
mina-naseh <mina.naseh98@gmail.com>
2024-11-11 15:01:46 +0100

    8_8_64

--- run_knn_benchmark.sh
+++ run_knn_benchmark.sh
@@ -3,8 +3,8 @@
 #SBATCH --output=benchmark_output_hpc.txt   # Output file
 #SBATCH --error=benchmark_error_hpc.txt     # Error file
 #SBATCH --nodes=1                           # Number of nodes
-#SBATCH --ntasks=16                          # Number of MPI tasks (processes)
-#SBATCH --cpus-per-task=2                  # CPU cores per MPI task (for threading)
+#SBATCH --ntasks=8                          # Number of MPI tasks (processes)
+#SBATCH --cpus-per-task=8                   # CPU cores per MPI task (for threading)
 #SBATCH --time=05:00:00                     # Maximum runtime (hh:mm:ss)
 #SBATCH --partition=batch                   # Partition/queue name
 
@@ -16,7 +16,7 @@ source .venv/bin/activate
 
 # Set the number of benchmarking runs and threads per MPI task
 RUNS=30
-THREADS=2
+THREADS=8
 
 # Run the benchmarking script with the specified arguments
-mpiexec -n 16 python benchmarking.py --num_runs $RUNS --num_threads $THREADS
+mpiexec -n 8 python benchmarking.py --num_runs $RUNS --num_threads $THREADS
